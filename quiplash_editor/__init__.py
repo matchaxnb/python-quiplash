ca05b816e2e5df7bf224c24998c854b012bc3a6a
Matcha <3207830+matchaxnb@users.noreply.github.com>
2023-09-12 14:41:34 +0200

    feat(quiplash_editor): add main function with broad conversion systems

--- quiplash_editor/__init__.py
+++ quiplash_editor/__init__.py
@@ -5,27 +5,29 @@ from dataclasses import dataclass, asdict
 from typing import List
 import csv
 
-SAFETY_QUIPS_DEFAULT = [
-    'jus de gouille',
-    'le buszy',
-    'jean-marc'
-]
+SAFETY_QUIPS_DEFAULT = ["jus de gouille", "le buszy", "jean-marc"]
+
+
 class RowImportError(Exception):
     """Cannot import a row"""
+
 
 class UnknownPromptVersionError(Exception):
     """The given prompt is of an unsupported version"""
 
+
 class UnsupportedConversionError(Exception):
     """Cannot convert prompt between these versions"""
-    
+
+
 def infer_prompt_version(prompt: dict):
-    if 'includes_player_name' in prompt:
+    if "includes_player_name" in prompt:
         return 3
-    elif all(a in prompt for a in ('id', 'prompt', 'x')):
+    elif all(a in prompt for a in ("id", "prompt", "x")):
         return 1
     else:
         raise UnknownPromptVersionError(f"Cannot identify {prompt}")
+
 
 def as_csv_dict(obj, *args, dict_factory=dict):
     """Convert dataclass item to CSV-friendly dict"""
@@ -40,9 +42,11 @@ def as_csv_dict(obj, *args, dict_factory=dict):
         del dct[field_name]
     return dct
 
+
 @dataclass(kw_only=True)
 class QuipPromptBase:
     """Base class for Quiplash prompts with utility functions to convert between versions"""
+
     id: int
     x: bool
     prompt: str
@@ -52,14 +56,14 @@ class QuipPromptBase:
         return QuipPromptV1(
             id=int(self.id),
             x=self.x,
-            prompt=self.prompt.replace('<ANYPLAYER>', '<ANY PLAYER>')
+            prompt=self.prompt.replace("<ANYPLAYER>", "<ANY PLAYER>"),
         )
-    
+
     def to_quiplashv3(self):
         """Provide a QuipPromptV3 version of a prompt"""
         if isinstance(self, QuipPromptV1):
-            prompt = self.prompt.replace('<ANY PLAYER>', '<ANYPLAYER>')
-            includes_player_name = '<ANYPLAYER>' in prompt
+            prompt = self.prompt.replace("<ANY PLAYER>", "<ANYPLAYER>")
+            includes_player_name = "<ANYPLAYER>" in prompt
             return QuipPromptV3(
                 id=str(self.id),
                 x=self.x,
@@ -70,7 +74,7 @@ class QuipPromptBase:
             )
         else:
             return QuipPromptV3(**asdict(self))
-        
+
     def to_version(self, target_version=None):
         """Convert Quiplash prompt to target version"""
         if target_version is None:
@@ -80,44 +84,54 @@ class QuipPromptBase:
         elif target_version == 3:
             return self.to_quiplashv3()
         else:
-            raise UnknownPromptVersionError(f"Cannot convert to unknown target_version {target_version}")
+            raise UnknownPromptVersionError(
+                f"Cannot convert to unknown target_version {target_version}"
+            )
+
+
 @dataclass(kw_only=True)
 class QuipPromptV1(QuipPromptBase):
     """A Quiplash quip prompt for Quiplash 1 & 2"""
+
     id: int
 
     @classmethod
     def from_pet_entry(cls, entry: dict):
         """build a QuipPromptV1 from a .pet entry
-        
+
         Takes care of converting whatever is missing"""
         ver = infer_prompt_version(entry)
         if ver == 1:
-            return cls(
-                id=int(entry['id']),
-                prompt=entry['prompt'],
-                x=bool(entry['x'])
-            )
+            return cls(id=int(entry["id"]), prompt=entry["prompt"], x=bool(entry["x"]))
         elif ver == 3:
             return cls(
-                id=int(entry['id']),
-                prompt=entry['prompt'].replace('<ANYPLAYER>', '<ANY PLAYER>'),
-                x=bool(entry['prompt'])
+                id=int(entry["id"]),
+                prompt=entry["prompt"].replace("<ANYPLAYER>", "<ANY PLAYER>"),
+                x=bool(entry["prompt"]),
             )
         else:
-            raise UnsupportedConversionError(f"Can't convert to QuipPromptV1 from ver {ver}")
+            raise UnsupportedConversionError(
+                f"Can't convert to QuipPromptV1 from ver {ver}"
+            )
 
     def to_pet_entry(self):
         """Convert QuipPromptV1 object to entry in Quiplash and Quiplash 2 PET format"""
-        return {
-            "id": self.id,
-            "prompt": self.prompt,
-            "x": self.x
-        }
+        return {"id": self.id, "prompt": self.prompt, "x": self.x}
+
+    @classmethod
+    def from_csv_entry(cls, entry: dict):
+        """build a QuipPromptV1 from a CSV entry"""
+        return cls(
+            id=int(entry.get("id")),
+            prompt=entry.get("prompt").replace("<ANYPLAYER>", "<ANY PLAYER>"),
+            x=bool(entry.get("x")),
+        )
+
 
 @dataclass(kw_only=True)
 class QuipPromptV3(QuipPromptBase):
     """A Quiplash quip prompt for Quiplash 3"""
+
     id: str
     includes_player_name: bool
     safety_quips: List[str]
@@ -131,39 +145,48 @@ class QuipPromptV3(QuipPromptBase):
             "prompt": self.prompt,
             "safetyQuips": self.safety_quips,
             "us": self.us,
-            "x": self.x
+            "x": self.x,
         }
 
     @classmethod
     def from_pet_entry(cls, entry: dict):
         """build a QuipPromptV3 from a .pet entry
-        
+
         Takes care of converting whatever is missing"""
         q = cls(
-            id=entry.get('id'),
-            includes_player_name=entry.get('includesPlayerName', False),
-            prompt=entry.get('prompt').replace('<ANY PLAYER>', '<ANYPLAYER>'),
-            safety_quips=entry.get('safetyQuips', list(SAFETY_QUIPS_DEFAULT)),
-            us=entry.get('us', False),
-            x=entry.get('x')
-            )
-        q.includes_player_name = '<ANYPLAYER>' in q.prompt
+            id=entry.get("id"),
+            includes_player_name=entry.get("includesPlayerName", False),
+            prompt=entry.get("prompt").replace("<ANY PLAYER>", "<ANYPLAYER>"),
+            safety_quips=entry.get("safetyQuips", list(SAFETY_QUIPS_DEFAULT)),
+            us=entry.get("us", False),
+            x=entry.get("x"),
+        )
+        q.includes_player_name = "<ANYPLAYER>" in q.prompt
         return q
 
     @classmethod
     def from_csv_entry(cls, entry: dict):
         """build a QuipPromptV3 from a CSV entry"""
         return cls(
-            id=entry.get('id'),
-            includes_player_name=entry.get('includes_player_name'),
-            prompt=entry.get('prompt'),
-            safety_quips=[entry.get(f'safety_quip_{a}') for a in range(1, 4)],
-            us=entry.get('us'),
-            x=entry.get('x')
+            id=entry.get("id"),
+            includes_player_name=entry.get("includes_player_name"),
+            prompt=entry.get("prompt"),
+            safety_quips=[entry.get(f"safety_quip_{a}") for a in range(1, 4)],
+            us=entry.get("us"),
+            x=entry.get("x"),
         )
+
+
 class QuiplashReader:
     target_version = None
 
+    @property
+    def prompt_class(self):
+        """the QuiplashPrompt class we target. Default to v1"""
+        if self.target_version is None or self.target_version == 1:
+            return QuipPromptV1
+        elif self.target_version == 3:
+            return QuipPromptV3
     @property
     def quip_prompts(self):
         """the quip prompts that this reader holds"""
@@ -179,13 +202,13 @@ class QuiplashReader:
 
     def load_pet(self, path):
         """load a PET file"""
-        with open(path, 'rb') as fh:
+        with open(path, "rb") as fh:
             self._data = json.load(fh)
         self.read()
 
     def load_csv(self, path):
         """load a CSV file"""
-        with open(path, newline='', encoding='utf-8') as fh:
+        with open(path, newline="", encoding="utf-8") as fh:
             csvreader = csv.DictReader(fh)
             self._csv_fields = csvreader.fieldnames
             for row in csvreader:
@@ -195,13 +218,26 @@ class QuiplashReader:
         """load a row from a CSV file"""
         if self._csv_fields is None:
             raise RowImportError("load_csv_row: cannot load data")
-        
+        self.quip_prompts.append(self.prompt_class.from_csv_entry(row))
+
     def read(self):
         """load Quiplash file"""
 
     def serialize(self, to_version=None):
         """Generate a serialized .pet from the contents"""
-        return json.dumps({"content": [a.to_version(self.target_version).to_pet_entry() for a in self.quip_prompts]}, indent=2, ensure_ascii=False) + "\n"
+        return (
+            json.dumps(
+                {
+                    "content": [
+                        a.to_version(self.target_version).to_pet_entry()
+                        for a in self.quip_prompts
+                    ]
+                },
+                indent=2,
+                ensure_ascii=False,
+            )
+            + "\n"
+        )
 
     def serialize_to_csv(self, path, to_version=None):
         """Generate a CSV version of the contents, for edition"""
@@ -216,8 +252,8 @@ class QuiplashReader:
             quip_keys.remove(field_name)
             for i in range(1, len(first_quip[field_name] + 1)):
                 quip_keys.append(f"{new_field_name}{i}")
-        with open(path, 'w', newline='', encoding='utf-8') as csvfile:
-            csvwriter = csv.DictWriter(csvfile, dialect='excel', fieldnames=quip_keys)
+        with open(path, "w", newline="", encoding="utf-8") as csvfile:
+            csvwriter = csv.DictWriter(csvfile, dialect="excel", fieldnames=quip_keys)
             csvwriter.writeheader()
             for quip_prompt in self.quip_prompts:
                 csvwriter.writerow(as_csv_dict(quip_prompt))
@@ -235,19 +271,25 @@ class QuiplashReader:
         c = cls()
         c.load_csv(path)
         return c
+
+
 class QuiplashReaderV3(QuiplashReader):
     """A Quiplash reader for Quiplash3"""
+
     target_version = 3
 
     def read(self):
-        contents = self._data['content']
+        contents = self._data["content"]
         for item in contents:
             self._quip_prompts.append(QuipPromptV3.from_pet_entry(item))
 
+
 class QuiplashReaderV1(QuiplashReader):
     """A Quiplash reader for Quiplash and Quiplash 2"""
+
     target_version = 1
+
     def read(self):
-        contents = self._data['content']
+        contents = self._data["content"]
         for item in contents:
             self._quip_prompts.append(QuipPromptV1.from_pet_entry(item))
--- quiplash_editor/__main__.py
+++ quiplash_editor/__main__.py
@@ -1,14 +1,71 @@
-from . import QuiplashReaderV3
+"""take the given PET file (first argument), copy it as a QuiplashV3 .pet file, and as a CSV file"""
+from . import QuiplashReaderV3, QuiplashReaderV1
 import sys
 
-qpath = sys.argv[1]
-output = sys.argv[2]
-reader = QuiplashReaderV3(qpath)
+# qpath = sys.argv[1]
+# output = sys.argv[2]
+# reader = QuiplashReaderV3(qpath)
 
 # print(reader.quip_prompts)
-print(reader.serialize())
-with open(output, 'w', encoding='utf-8') as fh:
-    fh.write(reader.serialize())
+# print(reader.serialize())
+# with open(output, "w", encoding="utf-8") as fh:
+#     fh.write(reader.serialize())
+# 
+# reader.serialize_to_csv(output + ".csv")
 
-reader.serialize_to_csv(output + ".csv")
+def to_v3_pet(orig_file, v3_output):
+    """Convert to V3 PET """
+    reader = QuiplashReaderV3(orig_file)
+    with open(v3_output, "w", encoding="utf-8") as fh:
+        fh.write(reader.serialize())
 
+def to_v3_csv(orig_file, v3_csv_output):
+    """Convert to V3 CSV"""
+    reader = QuiplashReaderV3(orig_file)
+    reader.serialize_to_csv(v3_csv_output, to_version=3)
+
+def to_v1_pet(orig_file, v1_output):
+    """Convert to V3 PET """
+    reader = QuiplashReaderV1(orig_file)
+    with open(v1_output, "w", encoding="utf-8") as fh:
+        fh.write(reader.serialize())
+
+def to_v1_csv(orig_file, v1_csv_output):
+    """Convert to V3 CSV"""
+    reader = QuiplashReaderV1(orig_file)
+    reader.serialize_to_csv(v1_csv_output, to_version=1)
+
+def csv_to_v1(csv_file, v1_output):
+    """Convert CSV file to V1 PET"""
+    reader = QuiplashReaderV1.from_csv_file(csv_file)
+    with open(v1_output, "w", encoding="utf-8") as fh:
+        fh.write(reader.serialize(to_version=1))
+
+def csv_to_v3(csv_file, v3_output):
+    """Convert CSV file to V1 PET"""
+    reader = QuiplashReaderV3.from_csv_file(csv_file)
+    with open(v3_output, "w", encoding="utf-8") as fh:
+        fh.write(reader.serialize(to_version=3))
+
+fun_map = {
+    'to_v3_pet': to_v3_pet,
+    'to_v3_csv': to_v3_csv,
+    'to_v1_pet': to_v1_pet,
+    'to_v1_csv': to_v1_csv,
+    'csv_to_v1': csv_to_v1,
+    'csv_to_v3': csv_to_v3,
+}
+
+try:
+    request = sys.argv[1]
+except IndexError:
+    request = None
+if request not in fun_map:
+    raise ValueError(f"Operation not supported: must be one of {'|'.join(list(fun_map.keys()))}")
+
+try:
+    the_in, the_out = sys.argv[2], sys.argv[3]
+except IndexError as e:
+    raise ValueError(f"Usage: {request} IN_FILE OUT_FILE") from e
+
+fun_map[request](the_in, the_out)
